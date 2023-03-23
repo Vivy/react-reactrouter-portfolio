d9032e6d5ca1abaab15392a4243958bedc0c4c4f
Vivy <Tomitaviorel@yahoo.com>
2023-03-23 13:58:46 +0200

    feat: gallery component done

--- src/pages/gallery/gallery.jsx
+++ src/pages/gallery/gallery.jsx
@@ -1,7 +1,33 @@
+import PagesHeader from '../../component/pagesheader/pagesheader';
 import * as S from './gallery.style';
+import HeaderImage from '/images/header_bg_3.jpg';
 
 const Gallery = () => {
-  return <S.Gallery>Gallery</S.Gallery>;
+  const galleryLength = 15;
+  const images = [];
+  for (let i = 1; i <= galleryLength; i++)
+    images.push(`/images/gallery${i}.jpg`);
+
+  return (
+    <>
+      <PagesHeader title='Our Galeery' image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias odio
+        facilis quisquam voluptate! Ad nisi beatae, minima dolorem doloribus
+        ducimus.
+      </PagesHeader>
+      <S.Gallery>
+        <S.Container>
+          {images.map((image, index) => {
+            return (
+              <article key={index}>
+                <img src={image} alt={`Gallery Image ${index + 1}`} />
+              </article>
+            );
+          })}
+        </S.Container>
+      </S.Gallery>
+    </>
+  );
 };
 
 export default Gallery;
--- src/pages/gallery/gallery.style.js
+++ src/pages/gallery/gallery.style.js
@@ -1,3 +1,35 @@
 import styled from 'styled-components';
 
-export const Gallery = styled.div``
+export const Gallery = styled.section``
+
+export const Container = styled.div`
+  width: var(--container--width-lg);
+  max-width: 1920px;
+  margin-inline: auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  article{
+    border: 1rem solid transparent;
+    transition:var(--transition);
+    
+    &:hover{
+      border-color:var(--color-gray-500);
+    }
+  }
+
+  @media screen and (max-width:1024px){
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+
+    article{
+      border: 0;
+    }
+  }
+
+  @media screen and (max-width:600px){
+    grid-template-columns: 1fr;
+    gap: 1rem;
+  }
+
+`
